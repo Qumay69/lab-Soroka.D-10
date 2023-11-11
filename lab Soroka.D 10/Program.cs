87575dea069e4ffaac1d5dd79172ca8a20d6208a
Sorok <Sorok@DESKTOP-C9GGTD7>
2023-11-11 22:48:58 +0200

    Добавьте файлы проекта.

---
+++ lab Soroka.D 10/Program.cs
@@ -0,0 +1,28 @@
+﻿//Сложность Сложная Вариант 1
+
+char[] array = Console.ReadLine().ToCharArray();
+
+bool isPalindrome = IsPalindrome(array);
+
+if (isPalindrome)
+{
+    Console.WriteLine("Массив является палиндромом.");
+}
+else
+{
+    Console.WriteLine("Массив не является палиндромом.");
+}
+static bool IsPalindrome(char[] array)
+{
+    int length = array.Length;
+
+    for (int i = 0; i < length / 2; i++)
+    {
+        if (array[i] != array[length - 1 - i])
+        {
+            return false;
+        }
+    }
+
+    return true;
+}
